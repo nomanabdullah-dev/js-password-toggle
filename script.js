416c8595d95506330c5f025689d5325d49bc4cc0
nomanabdullah-dev <nomanabdullah.dev@gmail.com>
2025-01-20 23:55:55 +0600

    password weak or strong added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let eyeIcon = document.getElementById("eyeicon");
 let password = document.getElementById("password");
+let msg = document.getElementById("message");
+let str = document.getElementById("strenght");
 
 eyeIcon.onclick = function(){
     if(password.type == "password"){
@@ -10,3 +12,25 @@ eyeIcon.onclick = function(){
         eyeIcon.src = "eye-close.png"
     }
 }
+
+password.addEventListener('input', ()=> {
+    if(password.value.length > 0){
+        msg.style.display = "block";
+    }else{
+        msg.style.display = "none";
+    }
+    
+    if(password.value.length < 4){
+        str.innerHTML = "weak";
+        password.style.borderColor = "#ff5025";
+        msg.style.color = "#ff5025";
+    }else if(password.value.length >= 4 && password.value.length < 8){
+        str.innerHTML = "medium";
+        password.style.borderColor = "yellow";
+        msg.style.color = "yellow";
+    }else if(password.value.length >= 8){
+        str.innerHTML = "strong";
+        password.style.borderColor = "#26d730";
+        msg.style.color = "#26d730";
+    }
+});
